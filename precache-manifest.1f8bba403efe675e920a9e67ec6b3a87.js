40765ffa87e92903bf65db41ea10e2373a54d78e
Floyd Jones <floydtjones@gmail.com>
2019-04-30 20:42:13 -0500

    Updates

---
+++ precache-manifest.1f8bba403efe675e920a9e67ec6b3a87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a24cdb901823ffcdac9087677188bf9",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "49e57d8dadd0f6875ea7",
+    "url": "/clickygame/static/css/main.3ce93b7c.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "49e57d8dadd0f6875ea7",
+    "url": "/clickygame/static/js/main.e001bfae.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
